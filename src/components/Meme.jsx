153a7c9e8c05f5c400018b7d97b2aa09bacb7faa
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-10-09 16:42:18 +0300

    Added form, inputs, and button elements to the Meme component for meme generation functionality. Also added corresponding CSS styles

---
+++ src/components/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme(){
+    return(
+        <form action="" id='meme-generator'>
+            <div className="inputs">
+                <input type="text" name="" id="" placeholder="Shut up"/>
+                <input type="text" name="" id="" placeholder="and take my money"/>
+            </div>
+            <button id='get-meme-btn'>Get a new meme image 🎨</button>
+        </form>
+    )
+}
