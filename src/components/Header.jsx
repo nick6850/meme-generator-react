02f7c9fd5200f571af2dbd73cbebae252b2cd54f
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-10-09 16:05:20 +0300

    Added Vite React setup and implemented Header component for the project.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../trollface-logo.png'
+
+export default function Headers() {
+    return(
+        <header>
+            <img src={logo} alt="trollface logo"/>
+            <h2>Meme Generator</h2>
+        </header>
+    )
+}
