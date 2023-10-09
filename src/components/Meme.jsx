d8706890a4424b40083d9a2e373e4a01fb1a5705
nick6850 <137728530+nick6850@users.noreply.github.com>
2023-10-10 02:20:33 +0300

    Implemented Meme component with dynamic image generation
    This commit adds the Meme component, which utilizes React's useState hook to manage state. The component generates a random meme image from the memeData imported from "../memeData".

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
+import memeData from "../memeData"
+
 export default function Meme(){
+    
+    const [newMeme, setMeme] = useState({
+        topText:'', 
+        bottomText:'', 
+        randomImage:'http://i.imgflip.com/1bij.jpg'
+    })
+
+    const [allMemeImages, setAllMemeImages] = useState(memeData)
+    
+    function getMeme(){
+        let randomNumber = Math.floor(Math.random() * 100)
+        setMeme(prev => {
+            return {
+            ...prev,
+            randomImage: allMemeImages.data.memes[randomNumber].url
+        }})
+    }
+
+
     return(
-        <form action="" id='meme-generator'>
-            <div className="inputs">
-                <input type="text" name="" id="" placeholder="Shut up"/>
-                <input type="text" name="" id="" placeholder="and take my money"/>
+        <main>
+            <div action="" id='meme-generator'>
+                <div className="inputs">
+                    <input type="text" name="" id="" placeholder="Shut up"/>
+                    <input type="text" name="" id="" placeholder="and take my money"/>
+                </div>
+                <button type='button' onClick={getMeme}  id='get-meme-btn'>Get a new meme image 🎨</button>
+                <img className='meme-img' src={newMeme.randomImage} alt="" />
             </div>
-            <button id='get-meme-btn'>Get a new meme image 🎨</button>
-        </form>
+        </main>
     )
 }
